ba290f0ebf2b012155ec3af8d2e0cc8c5410bb5b
y0k01cq <yenninti.kumar@walmart.com>
2025-07-26 16:50:25 +0530

    created controllerfile

---
+++ HomeController.java
@@ -0,0 +1,13 @@
+package com.example.bankapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to BankApp running on EC2!";
+    }
+}
